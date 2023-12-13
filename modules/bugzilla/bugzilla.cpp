c71e5db4a802415988bd5eb17f88926e563c2da2
Cara Salter <cara@devcara.com>
2023-12-13 16:48:38 -0500

    Create regex matcher for bug messages
    based on "bug ([0-9]*)", which finds a bug reference anywhere in a
    message. pretty nifty!

--- modules/bugzilla/bugzilla.cpp
+++ modules/bugzilla/bugzilla.cpp
@@ -1,13 +1,22 @@
 #include <142bot/modules.hpp>
 #include "cpr/cpr.h"
+#include <cpr/response.h>
+#include <dpp/dispatcher.h>
+#include <dpp/dpp.h>
+#include <regex>
+#include <pcre.h>
 
 using json = nlohmann::json;
 
 class BugzillaModule: public Module {
   std::string bugzillaBaseUrl;
+  std::string bugzillaApiKey;
 public:
   BugzillaModule(Bot* creator, ModuleLoader* ml): Module(creator, ml) {
     ml->attach({I_OnMessage}, this);
+
+    this->bugzillaBaseUrl = creator->cfg.value("bugzilla_base", "https://bugzilla.example.com");
+    this->bugzillaApiKey = creator->cfg.value("bugzilla_api_key", "bad-key");
   }
 
   virtual std::string version() {
@@ -15,6 +24,39 @@ public:
   }
   virtual std::string description() {
     return "Work with bugzilla";
+  }
+
+
+  json bugzilla_get(const std::string route) {
+    bot->core->log(dpp::ll_debug, fmt::format("Bugzilla: making GET request to {}/{}", this->bugzillaBaseUrl, route));
+
+    cpr::Response r = cpr::Get(cpr::Url{fmt::format("{}/{}&api_key={}", this->bugzillaBaseUrl, route, this->bugzillaApiKey).c_str()},
+			       cpr::Header{{"accept": "application/json"}, {"Content-Type", "application/json"}});
+
+    bot->core->log(dpp::ll_trace, fmt::format("Bugzilla: made request. Code: {}", r.status_code));
+
+    if (r.status_code != 200) {
+      bot->core->log(dpp::ll_error, fmt::format("Bugzilla API error: {}", r.text));
+      throw std::exception();
+    }
+
+    return json::parse(r.text);
+  }
+
+
+
+  virtual bool OnMessage(const dpp::message_create_t &message, const std::string &clean_message, bool mentioned, const std::vector<std::string> &mentions) {
+    // check to see if a bug was mentioned
+    std::regex re("bug ([0-9]*)");
+    std::smatch match;
+
+    if (std::regex_search(clean_message, match, re) == true) {
+      // we found a bug thing!
+      bot->core->message_create(dpp::message(message.msg.channel_id, "Found a bug! " + match.str(1)));
+      return true;
+    }
+
+    return true;
   }
 
   
